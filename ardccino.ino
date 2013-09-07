780b28c6d9525df57916d50fd0b4fe4dd2e63f29
Gerardo García Peña <killabytenow@gmail.com>
2013-09-07 22:25:41 +0200

    - longer DCC zero - dcc msg pool only 3 bits (8 entries) - added dcc_msg_idle XOR'ed byte - bugfixed dccSendBuffer - improver DCC ISR - added dbg info in dccRefresh

--- ardccino.ino
+++ ardccino.ino
@@ -312,11 +312,11 @@ void pwmSwitchDirection(int ps)
 // DCC
 ///////////////////////////////////////////////////////////////////////////////
 
-#define DCC_CTC_ZERO    200
+#define DCC_CTC_ZERO    232
 #define DCC_CTC_ONE     116
 
 #define DCC_MSG_MAX           5
-#define DCC_BUFFER_POOL_BITS  5
+#define DCC_BUFFER_POOL_BITS  3
 #define DCC_BUFFER_POOL_MASK  ((char) ~(0xff << DCC_BUFFER_POOL_BITS))
 #define DCC_BUFFER_POOL_SIZE  (1 << DCC_BUFFER_POOL_BITS)
 
@@ -327,12 +327,12 @@ struct dcc_buffer_struct {
   unsigned char len;
   char reps;
 } dcc_buffer_pool[DCC_BUFFER_POOL_SIZE];
-byte dcc_msg_idle[] = { 0xff, 0x00 };
+byte dcc_msg_idle[] = { 0xff, 0x00, 0xff };
 
 struct dcc_buffer_struct *dccSendBuffer(byte *msg, unsigned char len)
 {
   unsigned int address, command;
-  byte *n, x;
+  byte   x;
   struct dcc_buffer_struct *selected_buffer;
   
   if(len < 2)
@@ -341,14 +341,14 @@ struct dcc_buffer_struct *dccSendBuffer(byte *msg, unsigned char len)
     fatal("Message too long");
   
   // extract address
-  n = msg;
-  address = *n++; // enough for 7-bit address, or broadcast (0x00) or idle packets (0xff)
+  address = msg[0]; // enough for 7-bit address, or broadcast (0x00) or idle packets (0xff)
   if(address & 0x80 && address != 0xff) {
     // yep! two-byte address
     if(len <= 2)
       fatal("Message too short (2 byte address but packet shorter)");
-    address = address << 8 | *n++;
+    address = address << 8 | msg[1];
   }
+  Serial.print("address=");Serial.println(address);
   
   // extract command
   //command = *n & 0b11100000;
@@ -359,38 +359,35 @@ struct dcc_buffer_struct *dccSendBuffer(byte *msg, unsigned char len)
   selected_buffer = NULL;
   for(char i = 0; i < DCC_BUFFER_POOL_SIZE; i++) {
     if(address == dcc_buffer_pool[i].address
-    || !dcc_buffer_pool[i].address)
+    || !dcc_buffer_pool[i].address) {
       dcc_buffer_pool[i].reps = 0;
+      dcc_buffer_pool[i].address = 0xffff;
+    }
     if(!selected_buffer && dcc_buffer_pool[i].reps < 0) {
       selected_buffer = dcc_buffer_pool + i;
     }
   }
+  //Serial.print("selected_buffer_id=");Serial.println(i);
+  Serial.print("selected_buffer=");Serial.println((int) selected_buffer);
   if(!selected_buffer)
     return NULL;
   
   // copy msg to selected buffer
-  memcpy(selected_buffer->msg, msg, len);
-  
-  n = msg;
-  for(char i = 0; i < len; i++)
-    x ^= *n++;
-  msg[len] = x;
+  x = 0;
+  for(unsigned char i = 0; i < len; i++)
+    x ^= selected_buffer->msg[i] = msg[i];
+  selected_buffer->msg[len] = x;
   selected_buffer->len = len + 1;
 //selected_buffer->command = command;
   selected_buffer->address = address;
-  selected_buffer->reps = 5;
+  selected_buffer->reps = 100;
   
   return selected_buffer;
 }
 
-void dccSendBuffer(struct dcc_buffer_struct *b)
-{
-  b->reps = 5;
-}
-
 unsigned int lat;
 unsigned dcc_excesive_lat;
-unsigned char dcc_last_msg_id;
+int dcc_last_msg_id;
 char msg_pending;
 byte *msg = NULL;
 #define DCC_STATE_PREAMBLE 0
@@ -416,13 +413,15 @@ ISR(TIMER1_COMPA_vect)
     // select next msg
     dcc_last_msg_id = (dcc_last_msg_id + 1) & DCC_BUFFER_POOL_MASK;
     cmsg = dcc_buffer_pool + dcc_last_msg_id;
-    if(cmsg->reps == 0)
+    if(cmsg->reps == 0) 
       cmsg->reps = -1;
     if(cmsg->reps > 0) {
       msg         = cmsg->msg;
       msg_pending = cmsg->len;
+      cmsg->reps--;
     } else {
       msg         = dcc_msg_idle;
+      dcc_msg_idle[3] = dcc_msg_idle[0] ^ dcc_msg_idle[1]  ^ dcc_msg_idle[2];
       msg_pending = sizeof(dcc_msg_idle);
     }
     dccCurrentBit = 0;
@@ -440,7 +439,7 @@ ISR(TIMER1_COMPA_vect)
       dccCurrentBit = 0x80;
       if(msg_pending <= 0) {
         msg = NULL;
-        dccCurrentBit = 15;
+        dccCurrentBit = 16;
         OCR1A = DCC_CTC_ONE; // last bit
       } else {
         OCR1A = DCC_CTC_ZERO; // more data will come
@@ -461,8 +460,6 @@ check_latency:
 void dccStart(void)
 {
   unsigned char sreg;
-  Serial.print("sizeof(dcc_msg_idle) = "); Serial.println(sizeof(dcc_msg_idle));
-  Serial.print("dcc_msg_idle = "); Serial.println((int) dcc_msg_idle);
   
   disable_interrupts();
 
@@ -509,17 +506,53 @@ void dccStop(void)
   enable_interrupts();
 }
 
+unsigned int lolol = 0;
+unsigned int dir = 0;
 void dccRefresh(void)
 {
   ansi_goto(1,20);
     Serial.print("msg_pending="); Serial.print((int) msg_pending);
     Serial.print("  \r\nmsg=");   Serial.print((int) msg);
-    Serial.print("  \r\nlat=");   Serial.print((int) lat); Serial.print("    \r\n\r\n");
+    Serial.print("  \r\nlat=");   Serial.print((int) lat);
+    Serial.print("  \r\ndcc_last_msg_id="); Serial.print((int) dcc_last_msg_id);
+    Serial.print("    \r\n\r\n");
+    
+  for(int i = 0; i < DCC_BUFFER_POOL_SIZE; i++) {
+    Serial.print("slot["); Serial.print(i); Serial.print("] = ");
+    Serial.print((int) dcc_buffer_pool[i].address); Serial.print("; ");
+    Serial.print((int) dcc_buffer_pool[i].reps);    Serial.println("   ");
+  }
+      
+  if(lolol++ >= 20) {
+    Serial.println("YEP!");
+    //byte msg[] = { 3, 0b10000000 };
+    //msg[1] |= dir ? 0b10010000 : 0;
+    byte msg[] = { 3, 0b01001000 };
+    msg[1] |= dir ? 0b00100000 : 0;
+    dir = !dir;
+    lolol = 0;
+    struct dcc_buffer_struct *b = dccSendBuffer(msg, sizeof(msg));
+    if(b) {
+    Serial.print("\r\nmsg.len = "); Serial.print(b->len); Serial.print("     ");
+    for(unsigned char i = 0; i < b->len; i++) {
+      Serial.print("\r\nmsg[");
+      Serial.print(i);
+      Serial.print("] = ");
+      Serial.print(b->msg[i]);
+      Serial.print("    ");
+    }
+    } else {
+      for(int i = 0; i < 5; i++)
+      Serial.println("                                              ");
+    }
+  } else {
+    Serial.print("----");
+  }
 
   if(dcc_excesive_lat) {
     Serial.print("dcc_excesive_lat = ");
     Serial.println(dcc_excesive_lat);
-    Serial.print("!!!       ");
+    Serial.println("!!!       ");
   }
 }
 
