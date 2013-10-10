53a396d25046ab291c416ba7cd59dc4f4add862b
Gerardo García Peña <killabytenow@gmail.com>
2013-10-10 18:48:55 +0200

    Converted to C++

--- pwm.cpp
+++ pwm.cpp
@@ -1,196 +1,212 @@
-///////////////////////////////////////////////////////////////////////////////
-// PWM
-///////////////////////////////////////////////////////////////////////////////
+/*****************************************************************************
+ * pwm.cpp
+ *
+ * PWM booster manager.
+ *
+ * ---------------------------------------------------------------------------
+ * ardccino - Arduino dual PWM/DCC controller
+ *   (C) 2013 Gerardo García Peña <killabytenow@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation; either version 3 of the License, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *   for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ *****************************************************************************/
 
-struct pwmOutput_struct {
-  int           pwmValTarget;
-  int           pwmValCurrent;
-  int           pwmAcc;
-  unsigned char pwmMode;
-} pwmOutput[BOOSTER_N];
+#include "pwm.h"
 
-#define PWM_OUTPUT_N              BOOSTER_N
-#define PWM_OUTPUT_MIN            60
-#define PWM_OUTPUT_MAX            (255 - PWM_OUTPUT_MIN)
-#define PWM_OUTPUT_ACCEL          7
-#define PWM_OUTPUT_MAX_ACCEL      40
-
-struct booster_mngr_struct pwmBoosterMngr =
-  { "PWM", pwmStart, pwmStop, pwmRefresh };
-
-void pwmStart(void)
+PwmMngr::PwmMngr(Booster *b, int n) : boosters(b), nboosters(n), name("PWM")
 {
-  disable_interrupts();
-
-  // CONFIGURE TIMERS
-  // timer 1
-  TCCR1A = (TCCR1A & 0b00001100)
-         | 0b00000001;  // Fast PWM 8-bit (!WGM11, WGM10)
-  TCCR1B = (TCCR1B & 0b11100000)
-         | 0x4          // clock prescaler 0x04 (16e6/256 = 62.5 khz)
-         | 0b00001000;  // Fast PWM 8-bit (!WGM13, WGM12)
-  OCR1A = OCR1B = 0;    // set compare registers to 0
-  TIMSK1 &= 0b11011000; // disable all timer 1 interrupts
-
-  // timer 2
-  TCCR2A = (TCCR2A & 0b00001100)
-         | 0b00000011;  // Fast PWM (WGM21, WGM20)
-  TCCR2B = (TCCR2B & 0b11110000)
-         | 0x6          // clock prescaler 0x6 (16e6/256 = 62.5 khz)
-         | 0b00000000;  // Fast PWM (!WGM22)
-  OCR2A = OCR2B = 0;    // set compare registers to 0
-  TIMSK2 &= 0b11111000; // disable all timer 2 interrupts
-  
-  // CONFIGURE OUTPUT PINS FOR DIRECT PWM OUTPUT
-  for(int b = 0; b < BOOSTER_N; b++) {
-    switch(booster[b].pwmSignalPin) {
-      case 9:  TCCR1A |= 0b10000000; break; // Clear OC1A on cmp match
-      case 10: TCCR1A |= 0b00100000; break; // Clear OC1B on cmp match
-      case 11: TCCR2A |= 0b10000000; break; // Clear OC2A on cmp match
-      case 3:  TCCR2A |= 0b00100000; break; // Clear OC2B on cmp match
-      default: fatal("Cannot configure output pin for PWM output");
-    }
-  }
-
-  //-------------------------------------------
-  // RESET PWM STATUS
-  for(int i = 0; i < BOOSTER_N; i++) {
-    memset(pwmOutput + i, 0, sizeof(struct pwmOutput_struct));
-    pwmOutput[i].pwmMode = PWM_OUTPUT_MODE_INERTIAL;
-  }
-
-  enable_interrupts();
 }
 
-void pwmStop(void)
+void PwmMngr::init(void)
 {
-  disable_interrupts();
+	uint8_t timers;
 
-  // disconnect all OC1A:B/OC2A:B pins and go to normal operations mode
-  TCCR1A = TCCR1A & 0b00001100;    TCCR2A = TCCR2A & 0b00001100;
-  TCCR1B = TCCR1B & 0b11100000;    TCCR2B = TCCR2B & 0b11110000;
-  OCR1A = OCR1B = 0;               OCR2A = OCR2B = 0;
-  TIMSK1 &= 0b11011000;            TIMSK2 &= 0b11111000;
+	// discover which timers must be configured
+	for(int b = 0; b < nboosters; b++) {
+		for(uint8_t i = 0; i < n; i++) {
+			switch(pwmSignalPin) {
+			case 9:
+			case 10:
+				timers |= (1 << 1)
+				break;
+			case 3:
+			case 11:
+				timers |= (1 << 2)
+				break;
+			default:
+				fatal("Cannot figure how to configure output pin for PWM output");
+			}
+		}
+	}
 
-  enable_interrupts();
+	disable_interrupts();
+
+	// CONFIGURE TIMERS
+	if(timers & (1 << 1)) {
+		// timer 1
+		TCCR1A = (TCCR1A & 0b00001100)
+		       | 0b00000001;  // Fast PWM 8-bit (!WGM11, WGM10)
+		TCCR1B = (TCCR1B & 0b11100000)
+		       | 0x4          // clock prescaler 0x04 (16e6/256 = 62.5 khz)
+		       | 0b00001000;  // Fast PWM 8-bit (!WGM13, WGM12)
+		OCR1A = OCR1B = 0;    // set compare registers to 0
+		TIMSK1 &= 0b11011000; // disable all timer 1 interrupts
+	}
+
+	if(timers & (1 << 2)) }
+		// timer 2
+		TCCR2A = (TCCR2A & 0b00001100)
+		       | 0b00000011;  // Fast PWM (WGM21, WGM20)
+		TCCR2B = (TCCR2B & 0b11110000)
+		       | 0x6          // clock prescaler 0x6 (16e6/256 = 62.5 khz)
+		       | 0b00000000;  // Fast PWM (!WGM22)
+		OCR2A = OCR2B = 0;    // set compare registers to 0
+		TIMSK2 &= 0b11111000; // disable all timer 2 interrupts
+	}
+
+	// CONFIGURE OUTPUT PINS FOR DIRECT PWM OUTPUT
+	for(int b = 0; b < nboosters; b++) {
+		switch(boosters[b].pwmSignalPin) {
+		case 9:  TCCR1A |= 0b10000000; break; // Clear OC1A on cmp match
+		case 10: TCCR1A |= 0b00100000; break; // Clear OC1B on cmp match
+		case 11: TCCR2A |= 0b10000000; break; // Clear OC2A on cmp match
+		case 3:  TCCR2A |= 0b00100000; break; // Clear OC2B on cmp match
+		default: fatal("Cannot configure output pin for PWM output");
+		}
+	}
+
+	//-------------------------------------------
+	// RESET PWM STATUS
+	for(int b = 0; b < nboosters; b++) {
+		memset(pwmOutput[b], 0, sizeof(struct pwmOutput_struct));
+		pwmOutput[i].inertial = true;
+	}
+
+	enable_interrupts();
 }
 
-void pwmRefreshPwmOutput(int sp)
+void PwmMngr::fini(void)
 {
-  switch(pwmOutput[sp].pwmMode) {
-    case PWM_OUTPUT_MODE_INERTIAL:
-      if(pwmOutput[sp].pwmValTarget != pwmOutput[sp].pwmValCurrent) {
-        int d, target_speed, acc_inc;
-        
-        // If target of different sign than current val then brake to zero...
-        // elsewhere go directly to the target speed
-        target_speed = pwmOutput[sp].pwmValCurrent && (pwmOutput[sp].pwmValTarget ^ pwmOutput[sp].pwmValCurrent) & ((int) 0x8000)
-                         ? 0
-                         : pwmOutput[sp].pwmValTarget;
-      
-        // calculate acceleration increment
-        acc_inc = target_speed > pwmOutput[sp].pwmValCurrent ? PWM_OUTPUT_ACCEL : -PWM_OUTPUT_ACCEL;
-        if(abs(pwmOutput[sp].pwmAcc) < PWM_OUTPUT_MAX_ACCEL)
-          pwmOutput[sp].pwmAcc += acc_inc;
-        
-        // calculate final speed
-        if(abs(target_speed - pwmOutput[sp].pwmValCurrent) < abs(pwmOutput[sp].pwmAcc)) {
-          d = (target_speed - pwmOutput[sp].pwmValCurrent) >> 1;
-          if(!d)
-            d = target_speed - pwmOutput[sp].pwmValCurrent;
-          pwmOutput[sp].pwmAcc = d;
-        } else {
-          d = pwmOutput[sp].pwmAcc;
-        }
-        pwmOutput[sp].pwmValCurrent += d;
-      
-        if(pwmOutput[sp].pwmValTarget == pwmOutput[sp].pwmValCurrent)
-          pwmOutput[sp].pwmAcc = 0;
-      }
-      break;
-      
-    case PWM_OUTPUT_MODE_DIRECT:
-      pwmOutput[sp].pwmValCurrent = pwmOutput[sp].pwmValTarget;
-      pwmOutput[sp].pwmAcc = 0;
-      break;
-        
-    default:
-      fatal("Unknwon pwm mode");
-  }
+	disable_interrupts();
 
-  //pwmPrintStatus(sp);
+	// disconnect all OC1A:B/OC2A:B pins and go to normal operations mode
+	TCCR1A = TCCR1A & 0b00001100;    TCCR2A = TCCR2A & 0b00001100;
+	TCCR1B = TCCR1B & 0b11100000;    TCCR2B = TCCR2B & 0b11110000;
+	OCR1A = OCR1B = 0;               OCR2A = OCR2B = 0;
+	TIMSK1 &= 0b11011000;            TIMSK2 &= 0b11111000;
 
-  digitalWrite(booster[sp].dirSignalPin, pwmOutput[sp].pwmValCurrent > 0);
-  unsigned char pwmvalue = PWM_OUTPUT_MIN + abs(pwmOutput[sp].pwmValCurrent);
-  switch(booster[sp].pwmSignalPin) {
-    case  3: OCR2B = pwmvalue; break;
-    case  9: OCR1A = pwmvalue; break;
-    case 10: OCR1B = pwmvalue; break;
-    case 11: OCR2A = pwmvalue; break;
-    default: fatal("Cannot write PWM output to pin");
-  }
+	enable_interrupts();
 }
 
-void pwmRefresh(void)
+void PwmMngr::booster_refresh(Booster *b)
 {
-  for(int sp = 0; sp < PWM_OUTPUT_N; sp++)
-    pwmRefreshPwmOutput(sp);
+	if(pwmOutput[sp].trgt_speed == pwmOutput[sp].curr_speed)
+		return;
+
+	if(pwmOutput[sp].inertial) {
+		// INERTIAL MODE
+		int d, trgt_speed, acc_inc;
+
+		// If target of different sign than current val then brake to zero...
+		// elsewhere go directly to the target speed
+		trgt_speed = b->curr_speed && (b->trgt_speed ^ b->curr_speed) & ((int) 0x8000)
+				? 0
+				: b->trgt_speed;
+
+		// calculate acceleration increment
+		acc_inc = trgt_speed > b->curr_speed
+				?  b->inc_accel
+				: -b->inc_accel;
+		if(abs(b->curr_accel) < b->max_accel)
+			b->curr_accel += acc_inc;
+
+		// calculate final speed
+		if(abs(trgt_speed - b->curr_speed) < abs(b->curr_accel)) {
+			d = (trgt_speed - b->curr_speed) >> 1;
+			if(!d)
+				d = trgt_speed - b->curr_speed;
+			b->curr_accel = d;
+		} else {
+			d = b->curr_accel;
+		}
+		b->curr_speed += d;
+
+		if(b->trgt_speed == b->curr_speed)
+			b->curr_accel = 0;
+	} else {
+		// DIRECT MODE
+		b->curr_speed = b->trgt_speed;
+		b->curr_accel = 0;
+	}
+
+	// UPDATE BOOSTER OUTPUT
+	digitalWrite(b->dirSignalPin, b->curr_speed > 0);
+	unsigned char pwmvalue = b->min_power + abs(b->curr_speed);
+	switch(b->pwmSignalPin) {
+	case  3: OCR2B = pwmvalue; break;
+	case  9: OCR1A = pwmvalue; break;
+	case 10: OCR1B = pwmvalue; break;
+	case 11: OCR2A = pwmvalue; break;
+	default: fatal("Cannot write PWM output to pin");
+	}
 }
 
-void pwmPrintStatus(int sp)
+void PwmMngr::refresh(void)
 {
-  Serial.print("PWM[");
-  Serial.print(sp);
-  Serial.print("] = [");
-  Serial.print(pwmOutput[sp].pwmValCurrent < 0 ? '-' : '+');
-  Serial.print(pwmOutput[sp].pwmValCurrent);
-  Serial.print("] -> [");
-  Serial.print(pwmOutput[sp].pwmValTarget < 0 ? '-' : '+');
-  Serial.print(pwmOutput[sp].pwmValTarget);
-  Serial.print("] (acc=");
-  Serial.print(pwmOutput[sp].pwmAcc);
-  Serial.println(")");
+	for(int b = 0; b < nboosters; b++)
+		pwmRefreshPwmOutput(boosters + b);
 }
 
-
-void pwmAccelerate(int ps, int v)
+void PwmMngr::accelerate(Booster *b, int v)
 {
-  if(ps < 0 || ps > BOOSTER_N)
-    fatal("pwmAccelerate: pwm out of bounds.");
-    
-  pwmOutput[ps].pwmValTarget =
-          v > 0
-              ? (pwmOutput[ps].pwmValTarget + v < PWM_OUTPUT_MAX
-                  ? pwmOutput[ps].pwmValTarget + v
-                  : PWM_OUTPUT_MAX)
-              : (pwmOutput[ps].pwmValTarget + v > -PWM_OUTPUT_MAX
-                  ? pwmOutput[ps].pwmValTarget + v
-                  : -PWM_OUTPUT_MAX);
+	b->trgt_speed = v > 0
+			? (b->trgt_speed + v < b->max_power
+				? b->trgt_speed + v
+				: b->max_power)
+			: (b->trgt_speed + v > -b->max_power
+				? b->trgt_speed + v
+				: -b->max_power);
 }
 
-void pwmSpeed(int ps, int s)
+void PwmMngr::accelerate(int b, int v)
 {
-	if(ps < 0 || ps > BOOSTER_N)
+	if(b < 0 || b > nboosters)
+		fatal("pwmAccelerate: booster out of bounds.");
+
+	accelerate(boosters + b, v);
+}
+
+void PwmMngr::speed(int b, int s)
+{
+	if(b < 0 || b > BOOSTER_N)
 		fatal("pwmSpeed: pwm booster out of bounds.");
 	if(s < -255 || s > 255)
 		fatal("pwmSpeed: speed out of bounds.");
-
-	pwmOutput[ps].pwmValTarget = s;
+	boosters[b]->trgt_speed = s;
 }
 
-void pwmStop(int sp)
+void PwmMngr::stop(int b)
 {
-  if(sp < 0 || sp > PWM_OUTPUT_N)
-    fatal("pwmStop: pwm out of bounds");
-
-  pwmOutput[sp].pwmValTarget = 0;
+  speed(b, 0);
 }
 
-void pwmSwitchDirection(int ps)
+void PwmMngr::switch_dir(int b)
 {
-  if(ps < 0 || ps > BOOSTER_N)
-    fatal("pwmSwitchDirection: pwm out of bounds.");
+  if(b < 0 || b > nboosters)
+    fatal("switch_dir: pwm out of bounds.");
     
-  pwmOutput[ps].pwmValTarget = 0 - pwmOutput[ps].pwmValTarget;
+  boosters[b]->trgt_speed = 0 - boosters[b]->trgt_speed;
 }
 
--- pwm.h
+++ pwm.h
@@ -1,14 +1,51 @@
+/*****************************************************************************
+ * pwm.h
+ *
+ * PWM booster manager.
+ *
+ * ---------------------------------------------------------------------------
+ * ardccino - Arduino dual PWM/DCC controller
+ *   (C) 2013 Gerardo García Peña <killabytenow@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation; either version 3 of the License, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *   for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ *****************************************************************************/
+
 #ifndef __PWM_H__
 #define __PWM_H__
 
+#include "booster.h"
 #include "booster_mngr.h"
 
-#define PWM_OUTPUT_MODE_DIRECT    0
-#define PWM_OUTPUT_MODE_INERTIAL  1
+class PwmMngr : public BoosterMngr {
+private:
+	void booster_refresh(Booster *b);
+	void accelerate(Booster *b, int v);
 
-extern struct booster_mngr_struct pwmBoosterMngr;
-void pwmAccelerate(int ps, int v);
-void pwmSpeed(int ps, int v);
-void pwmMode(int booster, int mode);
+public:
+	PwmMngr(Booster *b, int n);
+	void init(void);
+	void fini(void);
+	void refresh(void);
+
+	void accelerate(int b, int v);
+	void speed(int b, int s);
+	void stop(int b);
+	void switch_dir(int b);
+};
+
+extern PwmMngr pwm;
 
 #endif
