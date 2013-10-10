9d69bae52f3f561cc945bf9c0feea0a5ba4f9fd6
Gerardo García Peña <killabytenow@gmail.com>
2013-10-10 23:30:07 +0200

    Moving to C++

--- hwgui.h
+++ hwgui.h
@@ -26,10 +26,20 @@
 #ifndef __HWGUI_H__
 #define __HWGUI_H__
 
-// GENERIC UI SCREEN
-struct ui_screen {
-  struct ui_screen *(*on_event)(struct ui_screen *ui, int event);
-  int focus;
+#include <Arduino.h>
+
+class UIScreen {
+	static UIScreen *current;
+
+private:
+	bool focus;
+
+protected:
+	UIScreen *on_event(uint8_t event);
+
+public:
+	static void handle(void);
+	void send_event(uint8_t event);
 };
 
 #define UI_EVENT_IDLE           0
@@ -44,11 +54,5 @@ struct ui_screen {
 #define UI_EVENT_PRESSED_UP     17
 #define UI_EVENT_PRESSED_DOWN   18
 #define UI_EVENT_SELECT         20
-
-extern struct ui_screen *ui_curr;
-
-struct ui_screen *ui_config_global_evh(struct ui_config_global_struct *ui, int event);
-struct ui_screen *ui_hello_evh(struct ui_hello_struct *ui, int event);
-struct ui_screen *ui_pwm_evh(struct ui_pwm_struct *ui, int event);
 
 #endif
