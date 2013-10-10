07d8c0bb05680828334b0449e20629a09f07452f
Gerardo García Peña <killabytenow@gmail.com>
2013-10-10 18:51:09 +0200

    Added copyright header.

--- error.cpp
+++ error.cpp
@@ -1,13 +1,35 @@
-///////////////////////////////////////////////////////////////////////////////
-// ERROR HANDLING
-///////////////////////////////////////////////////////////////////////////////
+/*****************************************************************************
+ * error.cpp
+ *
+ * Some funcs for printing and logging errors and debug info.
+ *
+ * ---------------------------------------------------------------------------
+ * ardccino - Arduino dual PWM/DCC controller
+ *   (C) 2013 Gerardo García Peña <killabytenow@gmail.com>
+ *
+ *   This program is free software; you can redistribute it and/or modify it
+ *   under the terms of the GNU General Public License as published by the Free
+ *   Software Foundation; either version 3 of the License, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful, but
+ *   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ *   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ *   for more details.
+ *
+ *   You should have received a copy of the GNU General Public License along
+ *   with this program; if not, write to the Free Software Foundation, Inc.,
+ *   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ *
+ *****************************************************************************/
+
 #include <Arduino.h>
-#include "booster.h"
+#include "off.h"
 #include "error.h"
 
 void fatal(char *msg)
 {
-  boosterEmergencyStop();
+  off.enable();
   Serial.print("FATAL ERROR");
   if(msg) {
     Serial.print(": ");
