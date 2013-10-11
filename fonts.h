56e38cd97214350deb8ce5d4f887e6f937031ebd
Gerardo García Peña <killabytenow@gmail.com>
2013-10-11 09:13:16 +0200

    The 'tinyfont.h' include now also declares other UTFT fonts.

---
+++ fonts.h
@@ -0,0 +1,9 @@
+#ifndef __FONTS_H__
+#define __FONTS_H__
+
+extern uint8_t TinyFont[];
+extern uint8_t SmallFont[];
+extern uint8_t BigFont[];
+extern uint8_t SevenSegNumFont[];
+
+#endif
--- tinyfont.h
+++
@@ -1,6 +0,0 @@
-#ifndef __TINYFONT_H__
-#define __TINYFONT_H__
-
-extern uint8_t TinyFont[];
-
-#endif
