e765e4fd48645f7d6c4d4edc5118137dfc9271cd
Gerardo García Peña <killabytenow@gmail.com>
2014-03-20 23:29:49 +0100

    bugfix: create build lib directories

--- Simulator.mk
+++ Simulator.mk
@@ -1,3 +1,28 @@
+###############################################################################
+# Simulator.mk
+#
+# Build the simulator software. Don't call directly, use Makefile instead.
+#
+# ---------------------------------------------------------------------------
+# ardccino - Arduino dual PWM/DCC controller
+#   (C) 2013-2014 Gerardo García Peña <killabytenow@gmail.com>
+#
+#   This program is free software; you can redistribute it and/or modify it
+#   under the terms of the GNU General Public License as published by the Free
+#   Software Foundation; either version 3 of the License, or (at your option)
+#   any later version.
+#
+#   This program is distributed in the hope that it will be useful, but
+#   WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+#   or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+#   for more details.
+#
+#   You should have received a copy of the GNU General Public License along
+#   with this program; if not, write to the Free Software Foundation, Inc.,
+#   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+#
+###############################################################################
+
 ########################################################################
 # TARGET binary
 
@@ -69,21 +94,27 @@ $(OBJDIR)/libs/%.o: $(USER_LIB_PATH)/%.c
 	$(CC) -c $(CPPFLAGS) $(CFLAGS) $< -o $@
 
 $(OBJDIR)/libs/%.d: %.c
+	mkdir -p $(dir $@)
 	$(CC) -MM $(CPPFLAGS) $(CFLAGS) $< -MF $@ -MT $(@:.d=.o)
 
 $(OBJDIR)/libs/%.d: %.cc
+	mkdir -p $(dir $@)
 	$(CXX) -MM $(CPPFLAGS) $(CXXFLAGS) $< -MF $@ -MT $(@:.d=.o)
 
 $(OBJDIR)/libs/%.d: %.cpp
+	mkdir -p $(dir $@)
 	$(CXX) -MM $(CPPFLAGS) $(CXXFLAGS) $< -MF $@ -MT $(@:.d=.o)
 
 $(OBJDIR)/libs/%.d: %.S
+	mkdir -p $(dir $@)
 	$(CC) -MM $(CPPFLAGS) $(ASFLAGS) $< -MF $@ -MT $(@:.d=.o)
 
 $(OBJDIR)/libs/%.d: %.s
+	mkdir -p $(dir $@)
 	$(CC) -MM $(CPPFLAGS) $(ASFLAGS) $< -MF $@ -MT $(@:.d=.o)
 
 $(OBJDIR)/%.cpp: %.ino
+	mkdir -p $(dir $@)
 	$(ECHO) '#include "Ardsim.h"' > $@
 	$(CAT)  $< >> $@
 
